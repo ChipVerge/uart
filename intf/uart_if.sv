5a3175f3c14380bc93b581b5bc116763c96e6899
Foez Ahmed <foez.official@gmail.com>
2026-04-28 23:40:56 +0600

    Refactor UART interface: add semaphore for send/receive tasks and update testbench for improved data handling

--- intf/uart_if.sv
+++ intf/uart_if.sv
@@ -14,8 +14,8 @@ interface uart_if;
   /* verilog_format: off */
   int unsigned baud_rate    = 115200;   // bits per second
   int unsigned parity_type  = 0;        // 0:none 1:odd 2:even 3:mark 4:space
-  int unsigned stop_bits    = 1;        // number of stop bits
   int unsigned data_bits    = 8;        // number of data bits
+  int unsigned stop_bits    = 1;        // number of stop bits
   /* verilog_format: on */
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
@@ -26,12 +26,17 @@ interface uart_if;
                                                                                                    \
   bit ``__NAME__``_drive;                                                                          \
   bit ``__NAME__``_reg;                                                                            \
+  semaphore ``__NAME__``_send_sem = new(1);                                                        \
+                                                                                                   \
   assign ``__NAME__`` = ``__NAME__``_drive ? ``__NAME__``_reg : 1'bz;                              \
-  task automatic send_``__NAME__``(input int unsigned data,                                        \
-                                   input int unsigned _baud_rate = baud_rate,                      \
-                                   input int unsigned _parity_type = parity_type,                  \
-                                   input int unsigned _stop_bits = stop_bits,                      \
-                                   input int unsigned _data_bits = data_bits);                     \
+                                                                                                   \
+  task automatic send_``__NAME__``(                                                                \
+      input int unsigned data,                                                                     \
+      input int unsigned _baud_rate = baud_rate,                                                   \
+      input int unsigned _parity_type = parity_type,                                               \
+      input int unsigned _data_bits = data_bits,                                                   \
+      input int unsigned _stop_bits = stop_bits                                                    \
+    );                                                                                             \
                                                                                                    \
     realtime        _bit_time;                                                                     \
     bit             _parity_bit;                                                                   \
@@ -40,8 +45,8 @@ interface uart_if;
                                                                                                    \
     baud_rate   = _baud_rate;                                                                      \
     parity_type = _parity_type;                                                                    \
-    stop_bits   = _stop_bits;                                                                      \
     data_bits   = _data_bits;                                                                      \
+    stop_bits   = _stop_bits;                                                                      \
     _data       = data;                                                                            \
                                                                                                    \
     case (_parity_type)                                                                            \
@@ -65,6 +70,7 @@ interface uart_if;
       _queue.push_back('1);                                                                        \
     end                                                                                            \
                                                                                                    \
+    ``__NAME__``_send_sem.get(1);                                                                  \
     ``__NAME__``_drive <= '1;                                                                      \
                                                                                                    \
     while (_queue.size() > 0) begin                                                                \
@@ -73,13 +79,61 @@ interface uart_if;
     end                                                                                            \
                                                                                                    \
     ``__NAME__``_drive <= '0;                                                                      \
+    ``__NAME__``_send_sem.put(1);                                                                  \
+                                                                                                   \
+  endtask                                                                                          \
+                                                                                                   \
+  semaphore ``__NAME__``_recv_sem = new(1);                                                        \
+                                                                                                   \
+  task automatic recv_``__NAME__``(                                                                \
+      output int unsigned data,                                                                    \
+      output int unsigned parity_bit,                                                              \
+      input  int unsigned _baud_rate = baud_rate,                                                  \
+      input  int unsigned _parity_type = parity_type,                                              \
+      input  int unsigned _data_bits = data_bits                                                   \
+    );                                                                                             \
+                                                                                                   \
+    realtime        _bit_time;                                                                     \
+    bit             _parity_bit;                                                                   \
+    bit      [31:0] _data;                                                                         \
+                                                                                                   \
+    baud_rate   = _baud_rate;                                                                      \
+    parity_type = _parity_type;                                                                    \
+    data_bits   = _data_bits;                                                                      \
+    _data       = data;                                                                            \
+    _bit_time = 1s / _baud_rate;                                                                   \
+                                                                                                   \
+    ``__NAME__``_recv_sem.get(1);                                                                  \
+                                                                                                   \
+    do begin                                                                                       \
+      @(negedge ``__NAME__``);                                                                     \
+      #(_bit_time / 2);                                                                            \
+    end while (``__NAME__``);                                                                      \
+                                                                                                   \
+    for (int i = 0; i < _data_bits; i++) begin                                                     \
+      #(_bit_time);                                                                                \
+      _data[i] = ``__NAME__``;                                                                     \
+    end                                                                                            \
+                                                                                                   \
+    if (_parity_type != 0) begin                                                                   \
+      #(_bit_time);                                                                                \
+      _parity_bit = ``__NAME__``;                                                                  \
+    end                                                                                            \
+                                                                                                   \
+    #(_bit_time);                                                                                  \
+                                                                                                   \
+    data = _data;                                                                                  \
+    parity_bit = _parity_bit;                                                                      \
+                                                                                                   \
+    ``__NAME__``_recv_sem.put(1);                                                                  \
                                                                                                    \
   endtask                                                                                          \
 
 
   `UART_IF_METHODS(tx)
+
   `UART_IF_METHODS(rx)
 
-  `undef UART_IF_METHODS
+`undef UART_IF_METHODS
 
-endinterface
+                endinterface
